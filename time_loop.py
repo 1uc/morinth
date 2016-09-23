4ef7a73e8e5972b3df821660ae041167d570eb27
Luc Grosheintz <luc.grosheintz@gmail.com>
2016-09-23 08:49:36 +0200

    Cosmetic refactoring of `TimeLoop`.

--- time_loop.py
+++ time_loop.py
@@ -7,11 +7,8 @@ class TimeLoop(object):
 
     def __call__(self, u0, T):
         u = u0
-        t = 0.0
-        dt = self.single_step.pick_time_step(u)
-        dt = np.min(dt, T - t);
-
-        n_steps = 0
+        t, n_steps = 0.0, 0
+        dt = self.pick_time_step(u, t, T)
 
         while t < T:
             u = self.single_step(u, t, dt)
@@ -20,9 +17,12 @@ class TimeLoop(object):
             if n_steps % 10 == 0:
                 self.visualize(u)
 
-            dt = self.single_step.pick_time_step(u)
-            dt = np.min(dt, T - t);
+            dt = self.pick_time_step(u, t, T)
 
             n_steps += 1
 
         return u
+
+    def pick_time_step(self, u, t, T):
+        dt = self.single_step.pick_time_step(u)
+        return np.min(dt, T - t);
