ee9b00ed684165b0d7b861ba7de36b506663b69f
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-01-16 10:26:15 +0100

    Add Euler equations.

---
+++ euler.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+class Euler(object):
+    """Euler equations with gravity."""
+
+    def __init__(self, gamma, gravity, specific_gas_constant):
+        self.gamma = gamma
+        self.gravity = gravity
+        self.specific_gas_constant = specific_gas_constant
+
+    def flux(self, u, axis, p=None):
+        """Physical flux of the Euler equations."""
+        if p is None:
+            p = self.pressure(u)
+
+        v = u[axis+1,...]/u[0,...]
+
+        flux = v*u
+
+        flux[axis+1,...] += p
+        flux[3,...] += v*p
+
+        return flux
+
+    def source(self, u, axis):
+        """Physical source term of the Euler equations."""
+        source = np.empty_like(u)
+
+        source[0,...] = 0.0
+        source[1,...] = 0.0
+        source[2,...] = -u[0,...]*self.gravity
+        source[3,...] = -u[axis+1,...]*self.gravity
+
+        return source
+
+    def max_eigenvalue(self, u):
+        """Largest eigenvalue of the flux derivative, df/du."""
+        p = self.pressure(u)
+        return self.speed(u) + self.sound_speed(u, p)
+
+    def pressure(self, u):
+        return (self.gamma-1.0)*(u[3,...] - self.kinetic_energy(u))
+
+    def kinetic_energy(self, u):
+        return 0.5*(u[1,...]**2 + u[2,...]**2)/u[0,...]
+
+    def internal_energy(self, p):
+        return p/(self.gamma-1.0)
+
+    def speed(self, u):
+        """Norm of the velocity."""
+        return np.sqrt((u[1,...]**2 + u[2,...]**2)/u[0,...]**2)
+
+    def sound_speed(self, u, p):
+        """The speed of sound in the system."""
+        return np.sqrt(self.gamma*p/u[0,...])
+
+    def conserved_variables(self, primitive_variables):
+        """Return the conserved variables, given the primitive variables."""
+        pvars = primitive_variables
+        cvars = np.empty_like(pvars)
+        cvars[0,...] = pvars[0,...]
+        cvars[1,...] = pvars[0,...]*pvars[1,...]
+        cvars[2,...] = pvars[0,...]*pvars[2,...]
+        cvars[3,...] = self.internal_energy(pvars[3,...]) + self.kinetic_energy(cvars)
+
+        return cvars
---
+++ euler_test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from euler import Euler
+# from flux_test_shared import test_continuity
+
+# @pytest.fixture
+# def model():
+#     return Euler()
