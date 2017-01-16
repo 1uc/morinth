91e171c6a334362353f42da2ba2f146db4d7d25f
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-01-16 17:12:44 +0100

    Add visualization for Euler equations (colormaps).

--- visualize.py
+++ visualize.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class SimplePlotting(object):
+class PlottingBase(object):
     def __init__(self, grid, base_name):
         self.grid = grid
         self.base_name = base_name
@@ -19,11 +19,78 @@ class SimplePlotting(object):
         return pattern.format(self.n_plots)
 
 
-class SimpleGraph(SimplePlotting):
+class SimpleGraph(PlottingBase):
     def plot(self, u):
         plt.plot(self.grid.cell_centers, u[0,:,:])
 
 
-class SimpleColormap(SimplePlotting):
+class SimpleColormap(PlottingBase):
     def plot(self, u):
-        plt.contourf(self.grid.X, self.grid.Y, u[0,:,:])
+        plot.contourf(self.grid.X, self.grid.Y, u[0,...])
+
+
+class ColormapWithArrows(PlottingBase):
+    def __init__(self, grid, base_name):
+        super().__init__(grid, base_name)
+
+        self.contourf_kwargs = {'cmap': 'viridis'}
+        self.quiver_kwargs = {}
+
+
+    def plot(self, u):
+        plt.clf()
+
+        scalar = self.transform_scalar(u)
+        vector = self.transform_vector(u)
+
+        X, Y = self.grid.X, self.grid.Y
+        plt.contourf(X, Y, scalar, 50, **self.contourf_kwargs)
+        plt.colorbar()
+
+        quiv = plt.quiver(X[::10,::10],
+                          Y[::10,::10],
+                          vector[0,::10,::10],
+                          vector[1,::10,::10],
+                          **self.quiver_kwargs)
+
+
+        # plt.quiverkey(quiv, 1, -0.18, *quiver_label(max_speed(v)))
+
+    def transform_vector(self, u):
+        return u[1:3,...] / u[0,...]
+
+
+class DensityPlots(ColormapWithArrows):
+    def transform_scalar(self, u):
+        return u[0,...]
+
+class LogDensityPlots(ColormapWithArrows):
+    def transform_scalar(self, u):
+        return np.log10(u[0,...])
+
+
+class PressurePlots(ColormapWithArrows):
+    def __init__(self, grid, base_name, model):
+        super().__init__(grid, base_name)
+        self.model = model
+
+    def transform_scalar(self, u):
+        return self.model.pressure(u)
+
+
+class LogPressurePlots(PressurePlots):
+    def transform_scalar(self, u):
+        return np.log10(self.model.pressure(u))
+
+
+
+class EulerPlots(PlottingBase):
+    def __init__(self, grid, base_name, model):
+        self.density_plots = DensityPlots(grid, base_name + "-rho")
+        self.log_density_plots = LogDensityPlots(grid, base_name + "-rho")
+        self.log_pressure_plots = LogPressurePlots(grid, base_name + "-p", model)
+
+    def __call__(self, u):
+        self.density_plots(u)
+        self.log_density_plots(u)
+        self.log_pressure_plots(u)
