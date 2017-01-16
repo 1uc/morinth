17d60db8a1a768d4cee8e34a2ae70b751034a28e
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-01-16 17:12:26 +0100

    Add outflow boundary conditions.

--- boundary_conditions.py
+++ boundary_conditions.py
@@ -14,3 +14,17 @@ class Periodic(BoundaryCondition):
         if self.grid.n_dims == 2:
             u[:,:,:n_ghost] = u[:,:,-2*n_ghost:-n_ghost]
             u[:,:,-n_ghost:] = u[:,:,n_ghost:2*n_ghost]
+
+
+class Outflow(BoundaryCondition):
+    def __call__(self, u):
+        n_ghost = self.grid.n_ghost
+
+        shape = (u.shape[0],) + (1,) + (u.shape[2],)
+        u[:,:n_ghost,:] = u[:,n_ghost,:].reshape(shape)
+        u[:,-n_ghost:,:] = u[:,-n_ghost-1,:].reshape(shape)
+
+        if self.grid.n_dims == 2:
+            shape = u.shape[:2] + (1,)
+            u[:,:,:n_ghost] = u[:,:,n_ghost].reshape(shape)
+            u[:,:,-n_ghost:] = u[:,:,-n_ghost-1].reshape(shape)
