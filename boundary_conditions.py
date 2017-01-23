12327395dfb34af22a474a0ff66c8cb1baecc424
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-01-23 16:57:43 +0100

    Make periodic boundary conditions 1D friendly.

--- boundary_conditions.py
+++ boundary_conditions.py
@@ -8,8 +8,8 @@ class Periodic(BoundaryCondition):
     def __call__(self, u):
         n_ghost = self.grid.n_ghost
 
-        u[:,:n_ghost,:] = u[:,-2*n_ghost:-n_ghost,:]
-        u[:,-n_ghost:,:] = u[:,n_ghost:2*n_ghost,:]
+        u[:,:n_ghost,...] = u[:,-2*n_ghost:-n_ghost,...]
+        u[:,-n_ghost:,...] = u[:,n_ghost:2*n_ghost,...]
 
         if self.grid.n_dims == 2:
             u[:,:,:n_ghost] = u[:,:,-2*n_ghost:-n_ghost]
