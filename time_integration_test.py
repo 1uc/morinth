a996b58309ac399d01376cba165d2bea66a27fb6
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-01-26 17:19:33 +0100

    Update time-integration test-cases.

--- time_integration_test.py
+++ time_integration_test.py
@@ -6,55 +6,69 @@ from grid import Grid
 from boundary_conditions import Periodic
 from finite_volume_fluxes import FiniteVolumeFluxes
 from time_integration import BackwardEuler, BDF2, DIRKa23, DIRKa34
-from runge_kutta import ForwardEuler, SSP2, SSP3
+from runge_kutta import ForwardEuler, SSP2, SSP3, Fehlberg
 from time_loop import TimeLoop
 from time_keeper import FixedDuration, PlotNever
+from testing_tools import convergence_rate
 
-
-def test_forward_euler_init():
-    model = Burgers()
-    flux = Rusanov(model)
-    grid = Grid([0.0, 1.0], 100, 1)
-    fvm = FiniteVolumeFluxes(grid, flux)
-    bc = Periodic(grid)
-
-    forward_euler = ForwardEuler(bc, fvm)
-
-    assert forward_euler.bc is bc
-    assert forward_euler.rate_of_change is fvm
 
 class MockROC(object):
+    def __init__(self, dt):
+        self._dt = dt
+
     def __call__(self, u, t):
         return u
 
     def pick_time_step(self, u):
-        return 0.01
+        return self._dt
+
+def with_mask(Solver, mask, cfl_number):
+    return lambda bc, mock_roc: Solver(bc, mock_roc, mask, cfl_number)
 
 def test_mock_ode():
     bc = lambda x: None
-    mock_roc = MockROC()
+    plotting_steps = PlotNever()
     mask = np.array([True])
 
-    solvers = [ ForwardEuler(bc, mock_roc),
-                BackwardEuler(bc, mock_roc, mask, 3.0),
-                BDF2(bc, mock_roc, mask, fixed_dt = 0.05),
-                SSP2(bc, mock_roc),
-                SSP3(bc, mock_roc),
-                DIRKa23(bc, mock_roc, mask, 3.0),
-                DIRKa34(bc, mock_roc, mask, 3.0)]
+#     solvers = [ ForwardEuler(bc, mock_roc),
+#                 BackwardEuler(bc, mock_roc, mask, 3.0),
+#                 BDF2(bc, mock_roc, mask, fixed_dt = 0.05),
+#                 SSP2(bc, mock_roc),
+#                 SSP3(bc, mock_roc),
+#                 Fehlberg(bc, mock_roc),
+#                 DIRKa23(bc, mock_roc, mask, 3.0),
+#                 DIRKa34(bc, mock_roc, mask, 3.0)]
+    solvers = [ ForwardEuler,
+                with_mask(BackwardEuler, mask, 3.0),
+                # lambda bc, mock_roc: BDF2(bc, mock_roc, mask, fixed_dt = 0.05),
+                SSP2,
+                SSP3,
+                Fehlberg,
+                # with_mask(DIRKa23, mask, 3.0),
+                # with_mask(DIRKa34, mask, 3.0)
+                ]
 
-    tolerances = [0.013, 0.05, 0.008, 0.01, 0.004, 0.0082, 0.0017]
+    rates = [ 1.0, 1.0, 2.0, 2.0, 3.0, 5.0, 3.0, 4.0 ]
 
     T = 1.0
+    all_resolutions = [10, 20, 40]
+    error = np.empty(len(all_resolutions))
 
-    for single_step, tol in zip(solvers, tolerances):
-        plotting_steps = PlotNever()
-        time_loop = TimeLoop(single_step, lambda x: None, plotting_steps)
+    for Solver, expected_rate in zip(solvers, rates):
+        for k, res in enumerate(all_resolutions):
+            mock_roc = MockROC(T/res)
+            single_step = Solver(bc, mock_roc)
+            time_loop = TimeLoop(single_step, lambda x: None, plotting_steps)
 
-        u0 = np.array([1.0]).reshape((1, 1, 1))
-        uT = time_loop(u0, FixedDuration(T))
+            u0 = np.array([1.0]).reshape((1, 1, 1))
+            uT = time_loop(u0, FixedDuration(T))
 
-        assert np.all(np.abs(uT - u0*np.exp(T)) < tol), str(single_step)
+            u_ref = u0*np.exp(T)
+            # u_ref = u0 + np.array([T])
+            error[k] = np.abs(uT - u_ref)
+
+        observed_rate = np.abs(convergence_rate(error, np.array(all_resolutions)))
+        assert np.all(observed_rate - expected_rate > -0.1), str(single_step)
 
 if __name__ == '__main__':
     test_mock_ode()
