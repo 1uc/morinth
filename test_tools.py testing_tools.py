4b3dff1f230477ad50b4a07a7850f0c9bab3a4ea
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-01-26 17:18:18 +0100

    Start a toolkit of testing routines.

---
+++ test_tools.py testing_tools.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def convergence_rate(error, resolution):
+    return np.log(error[:-1]/error[1:])/np.log(resolution[:-1]/resolution[1:])
+
+def l1_error(approximate, reference):
+    """L1-error on a uniform grid."""
+
+    err = np.mean(np.abs(approximate - reference), axis=-1)
+    if approximate.ndim == 3:
+        err = np.mean(err, axis = -1)
+
+    return err
+
