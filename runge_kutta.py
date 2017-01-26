1719c99234a7bdd9b4b22506f6113b7386b53a04
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-01-26 17:17:05 +0100

    Add fifth order Fehlberg RK method.

--- runge_kutta.py
+++ runge_kutta.py
@@ -91,3 +91,18 @@ class SSP3(ExplicitRungeKutta):
 
         super().__init__(bc, rate_of_change, ButcherTableau(a, b))
         self.cfl_number = 0.40
+
+class Fehlberg(ExplicitRungeKutta):
+    """Fifth order scheme, not SSP"""
+
+    def __init__(self, bc, rate_of_change):
+        a = np.array([[ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                      [ 0.25, 0.0, 0.0, 0.0, 0.0, 0.0],
+                      [ 3.0/32.0, 9.0/32.0, 0.0, 0.0, 0.0, 0.0],
+                      [ 1932.0/2197.0, -7200.0/2197.0, 7296.0/2197.0, 0.0, 0.0, 0.0],
+                      [ 439.0/216.0, -8.0, 3680.0/513.0, -845.0/4104, 0.0, 0.0],
+                      [ -8.0/27.0, 2.0, -3544.0/2565.0, 1859.0/4104.0, -11.0/40.0, 0.0]])
+        b = np.array([16.0/135.0, 0.0, 6656.0/12825.0, 28561.0/56430.0, -9.0/50.0, 2.0/55.0])
+
+        super().__init__(bc, rate_of_change, ButcherTableau(a, b))
+        self.cfl_number = 0.3
