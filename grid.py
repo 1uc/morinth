20d5dd10c82746f1d3de534f6af2653d39e70766
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-02-01 09:54:22 +0100

    Make `grid.n_cells` less convoluted.

--- grid.py
+++ grid.py
@@ -5,7 +5,11 @@ class Grid(object):
 
     def __init__(self, domain, n_cells, n_ghost):
         self._domain = np.array(domain)
-        self._n_cells = n_cells
+
+        try:
+            self.n_cells = tuple(n_cells)
+        except:
+            self.n_cells = (n_cells,)
 
         self.n_dims = self._domain.ndim
         self.n_ghost = n_ghost
@@ -23,17 +27,15 @@ class Grid(object):
             raise Exception("Strange dimensions.")
 
     def make_boundary_mask(self):
-        if isinstance(self._n_cells, int):
-            shape = (1, self._n_cells, 1)
-        else:
-            shape = (1, self._n_cells[0], self._n_cells[1])
+        shape = (1,) + self.n_cells
 
         n_ghost = self.n_ghost
 
         mask = np.full(shape, True, dtype=bool)
-        mask[:,:n_ghost,:] = False
-        mask[:,-n_ghost:,:] = False
-        if len(shape) == 2:
+        mask[:,:n_ghost,...] = False
+        mask[:,-n_ghost:,...] = False
+
+        if self.n_dims == 2:
             mask[:,:,:n_ghost] = False
             mask[:,:,-n_ghost:] = False
 
@@ -54,7 +56,7 @@ class Grid(object):
         return x
 
     def make_1d_grid(self):
-        x = self.partition(self._domain[0], self._domain[1], self._n_cells)
+        x = self.partition(self._domain[0], self._domain[1], self.n_cells[0])
 
         self.edges = x.reshape((-1, 1))
         self.cell_centers = 0.5*(self.edges[1:,:] + self.edges[:-1,:])
@@ -62,8 +64,8 @@ class Grid(object):
         self.dx = self.edges[1,0] - self.edges[0,0]
 
     def make_2d_grid(self):
-        x = self.partition(self._domain[0,0], self._domain[0,1], self._n_cells[0])
-        y = self.partition(self._domain[1,0], self._domain[1,1], self._n_cells[1])
+        x = self.partition(self._domain[0,0], self._domain[0,1], self.n_cells[0])
+        y = self.partition(self._domain[1,0], self._domain[1,1], self.n_cells[1])
 
         self.edges = np.empty((x.shape[0], y.shape[0], 2))
         X, Y = np.meshgrid(x, y)
