c94eaba893953e06a26f2535a00d00bb899ecd35
Luc Grosheintz <luc.grosheintz@gmail.com>
2017-02-08 10:38:25 +0100

    Improved WENO tests.

--- weno_test.py
+++ weno_test.py
@@ -113,3 +113,70 @@ def test_weno_well_balanced():
         plt.plot(grid.cell_centers[3:-3,0], u0[0,3:-3], 'k_')
         plt.plot(grid.edges[3:-3,0], u_minus[0,:], '<')
         plt.show()
+
+def test_weno_well_balanced_order():
+    model = Euler(gamma = 1.4, gravity = 1.0, specific_gas_constant = 1.0)
+    all_resolutions = 2**np.arange(4, 10)
+
+    ic = GaussianBumpIC(model)
+    rho_ref = ic.rho_ref
+    p_ref = ic.p_ref
+    T_ref = model.temperature(rho=rho_ref, p=p_ref)
+    x_ref = ic.x_ref
+
+    err_plus = np.empty((4, len(all_resolutions)))
+    err_minus = np.empty((4, len(all_resolutions)))
+
+    for l, resolution in enumerate(np.nditer(all_resolutions)):
+        grid = Grid([0.0, 1.0], resolution, 3)
+        equilibrium = IsothermalEquilibrium(grid, model)
+        weno = OptimalWENO(EquilibriumStencil(grid, equilibrium, model))
+
+        ic.amplitude = 0.0
+        u0 = ic(grid)
+        u_plus, u_minus = weno(u0, axis=0)
+        w_plus = model.primitive_variables(u_plus)
+        w_minus = model.primitive_variables(u_minus)
+
+        x = grid.edges[3:-3,...,0]
+        w_exact = np.zeros_like(u_plus)
+        w_exact[0,...], w_exact[3,...] = equilibrium.extrapolate(p_ref, T_ref, x_ref, x)
+
+        err_plus[:,l] = linf_error(w_plus, w_exact)
+        err_minus[:,l] = linf_error(w_minus, w_exact)
+
+    assert np.all(err_plus < 1e-13)
+    assert np.all(err_minus < 1e-13)
+
+    rho_rate_plus = convergence_rate(err_plus[0,...], all_resolutions-6)
+    rho_rate_minus = convergence_rate(err_minus[0,...], all_resolutions-6)
+
+    p_rate_plus = convergence_rate(err_plus[3,...], all_resolutions-6)
+    p_rate_minus = convergence_rate(err_minus[3,...], all_resolutions-6)
+
+    print("")
+    print(rho_rate_plus)
+    print(rho_rate_minus)
+    print(p_rate_plus)
+    print(p_rate_minus)
+
+def test_reconstruction():
+    model = Euler(gamma = 1.4, gravity = 1.0, specific_gas_constant = 1.0)
+    grid = Grid([0.0, 1.0], 16, 3)
+    equilibrium = IsothermalEquilibrium(grid, model)
+
+    ic = GaussianBumpIC(model)
+    rho_ref = ic.rho_ref
+    p_ref = ic.p_ref
+    T_ref = model.temperature(rho=rho_ref, p=p_ref)
+    x_ref = ic.x_ref
+
+    u0 = ic(grid)
+
+    x = grid.cell_centers[...,0]
+    rho_exact, p_exact = equilibrium.extrapolate(p_ref, T_ref, x_ref, x)
+
+    rho_point, p_point, _, _, _ = equilibrium.point_values(u0)
+
+    print(rho_point - rho_exact)
+
